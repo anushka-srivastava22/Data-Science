82422f7686ba91a63b99f5d26b85b1884b96a7bc
anushka-srivastava22 <srivastavaanushka3@gmail.com>
2023-12-11 22:50:02 +0530

    first

---
+++ python-basics/datastructures.py
@@ -0,0 +1,18 @@
+# Data Structures - Used for storing collection of data
+
+#Lists -  Ordered and Mutable Collections
+my_list = [1,2,3,4]
+
+#Tuple - Ordered and Immutable Collections
+my_tuple = (1,2,3)
+
+#Dictionary - Immutable; Unordered Collections of Key Value Pair
+my_dict = {'name':'Anu', 'age':23}
+
+#Set - Unordered Collections of unique elements and is mutable
+my_set = {1,2,3}
+
+print(my_list, "\n")
+print(my_tuple, "\n")
+print(my_dict, "\n")
+print(my_set, "\n")
---
+++ python-basics/datatypes.py
@@ -0,0 +1,18 @@
+#Data Types
+
+#Integer - Whole number without decimal value 
+num1 = 22
+
+#Floats - Number with decimal parts
+num2 = 13.22
+
+#String - Textual data enclosed within double quotes
+msg = "Hello World!"
+
+#Boolean - Represent true and false.
+is_valid = True
+
+print(num1, "\n")
+print(num2, "\n")
+print(msg, "\n")
+print(is_valid, "\n")
